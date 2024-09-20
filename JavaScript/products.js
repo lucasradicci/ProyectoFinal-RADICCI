52064e41b0108b2ba20dfbcee8eedd64a36a2c9f
lucas radicci <radicci.lukas@gmail.com>
2024-09-20 15:52:05 -0300

    funcion para mostrar los productos y para iniciar el simulador

---
+++ JavaScript/products.js
@@ -0,0 +1,25 @@
+// Inicializar el simulador
+function loadProducts() {
+    $.getJSON('products.json')
+        .done(function(products) {
+            displayProducts(products);
+        })
+        .fail(function() {
+            displayMessage('Error al cargar productos');
+        });
+}
+
+// Mostrar productos en la interfaz
+function displayProducts(products) {
+    products.forEach(product => {
+        const productDiv = $(`
+            <div class="product">
+                <h3>${product.name}</h3>
+                <p>Marca: ${product.brand}</p>
+                <p>Precio: $${product.price}</p>
+                <button onclick="addToCart(${product.id})">Agregar al carrito</button>
+            </div>
+        `);
+        $('#product-list').append(productDiv);
+    });
+}
